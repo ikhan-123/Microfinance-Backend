a769bb44e0dbe169a1625ad56d74bfc90a80ff47
ikhan-123 <ifficodder@gmail.com>
2025-01-26 08:57:42 +0500

    still some bugs are not solved

--- src/index.js
+++ src/index.js
@@ -1,35 +1,21 @@
-// require('dotenv').config()
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-
 dotenv.config({
     path: "./env"
-})
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
 
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is running on port : ${process.env.PORT || 8000}`);
-
-        })
+        });
     })
     .catch((err) => {
         console.log("MONGO DB connection failed  !!!", err);
-
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
